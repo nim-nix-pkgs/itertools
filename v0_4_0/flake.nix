474af6ebe9ae885404b7874cbdcd17b48716040f
hugosenari <hugosenari@gmail.com>
2022-03-12 02:04:54 -0300

    chore: re index itertools

---
+++ v0_4_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Itertools for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-itertools-v0_4_0.flake = false;
+  inputs.src-itertools-v0_4_0.owner = "narimiran";
+  inputs.src-itertools-v0_4_0.ref   = "refs/tags/v0.4.0";
+  inputs.src-itertools-v0_4_0.repo  = "itertools";
+  inputs.src-itertools-v0_4_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-itertools-v0_4_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-itertools-v0_4_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
